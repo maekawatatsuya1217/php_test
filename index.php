704eb2c73c189f05c64d51677918165001571191
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-12 13:54:57 +0900

    定数について学習

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php
+  
+//   定数　変わらない数値
+  const MAX =  10;
+  echo MAX;
 
-  $test1 = "おはようございます！";
-  $test2 = "今日の天気は晴れです！";
-
-//   変数に値を再代入
-  $test1 = "こんにちは！";
-
-//   変数を出力
-  echo $test1;
-  echo $test2;
-
-//   変数を連結して出力
-  echo $test1.$test2;
-
-//   var_dump
-  echo var_dump($test1);
+//   定数　変わらない文字
+  const LANGUAGE = "テスト";
+  echo LANGUAGE;
 
 ?>
