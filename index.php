08e6d91766d30245c24c46c3808aaaad32734d1c
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-12 11:19:03 +0900

    変数についての学習

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-  echo "MAMPを初体験";
+
+  $test1 = "おはようございます！";
+  $test2 = "今日の天気は晴れです！";
+
+//   変数に値を再代入
+  $test1 = "こんにちは！";
+
+//   変数を出力
+  echo $test1;
+  echo $test2;
+
+//   変数を連結して出力
+  echo $test1.$test2;
+
+//   var_dump
+  echo var_dump($test1);
+
 ?>
