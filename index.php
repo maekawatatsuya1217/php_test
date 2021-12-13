ce600e85c9039466c5a34a3d562b4dc3f13ca857
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-13 20:51:19 +0900

    複雑な連想配列を学習

--- index.php
+++ index.php
@@ -21,4 +21,34 @@
 
   //   配列の中から特定の要素を出力
   var_dump($array_2);
+
+//   連想配列
+
+    $array_3 = [
+    'name' => '前川',
+    'height' => 170,
+    'hobby' => 'バドミントン'
+    ];
+
+    echo $array_3['name'];
+
+    var_dump($array_3);
+
+    // 複雑な連想配列
+
+    $player = [
+        '本田選手' => [
+            'height' => 180,
+            'hobby' => 'サッカー'
+        ],
+        '香川選手' => [
+            'height' => 170,
+            'hobby' => 'サッカー'
+        ],
+    ];
+
+    echo $player['本田選手']['height'];
+
+    var_dump($player);
+
 ?>
