543f5883204ca063485a837c0520e4400e60fba8
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-14 22:01:10 +0900

    foreach文

--- index.php
+++ index.php
@@ -1,44 +1,13 @@
 <?php
 
-  $height = 180;
+  $members = [
+    'name' => '本田',
+    'height' => 170,
+    'age' => 28 
+  ];
 
-  if ($height == 180){
-      echo '身長は.{$height}.cmです！';
-  }
-
-  $color = 'blue';
-
-  if ($color == red){
-      echo '信号は赤です。';
-  } elseif ($color == yellow){
-      echo '信号は黄色です。';
-  } else {
-      echo '信号は赤色です。';
-  }
-
-//   emptyを使用したif文
-
-  $num = '';
-
-  if (empty($num)){
-      echo '変数は空です';
-  } else{
-      echo '変数は空ではありません';
-  }
-
-  if (!empty($num)){
-    echo '変数は空ではありません！';
-  }
-
-  $color1 = 'yellow';
-  $color2 = 'red';
-
-  if ($color1 === 'blue' && $color2 === 'red'){
-      echo '変数の中身が２つとも一致しています！';
-  } elseif ($color1 === 'blue' || $color2 === 'red'){
-      echo '変数の中身が1つだけ一致しています！';
-  } else {
-      echo '変数の中身は全く一致していません！';
+  foreach ($members as $key => $value){
+      echo '{$key}は{$value}です！';
   }
 
 ?>
