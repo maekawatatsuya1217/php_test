ae0b923f95b20e76410d0de1f6e2451fb41eeeee
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-16 15:05:48 +0900

    ユーザー定義関数

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
-  $in_data =5;
 
-  switch($in_data){
-      case 1:
-        echo '変数は1です';
-        break;
-      case 2:
-        echo '変数は2です';
-        break;
-      case 3:
-        echo '変数は3です';
-        break;
-      default:
-        echo '変数は1から3の数字ではありません！';
+  function name($name){
+      echo $name;
   }
+  name('名前');
+
+  function sumPrice($int1, $int2){
+      $int3 = $int1 + $int2;
+      return $int3;
+  }
+
+  $total = sumPrice(3,5);
+
+  echo $total;
 ?>
