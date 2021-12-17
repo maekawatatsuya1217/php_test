a1a943cb010de9a06551cbef1a2df6f7ca6e2f9c
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-17 16:20:49 +0900

    戻るボタン

--- form/input.php
+++ form/input.php
@@ -26,10 +26,10 @@
     <?php if($pageFlag === 0) : ?>
         <form method="POST" action="input.php">
             名前
-            <input type="text" name="your_name">
+            <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'];}?>">
             <br>
             メールアドレス
-            <input type="email" name="your_email">
+            <input type="email" name="your_email" value="<?php if(!empty($_POST['your_email'])){echo $_POST['your_email'];}?>">
             <br>
             <input type="submit" name="btn_confilm" value="確認画面へ">
         </form>
@@ -39,6 +39,7 @@
     <!-- 確認画面 -->
     <?php if($pageFlag === 1) :?>
         <form method="POST" action="input.php">
+            <input type="submit" name="back" value="戻る">
             名前
             <?php echo $_POST['your_name'];?>
             <br>
