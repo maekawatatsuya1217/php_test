0fed27d47a5a22cc1ff8fe21f882f29d2b0bde11
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-21 13:51:45 +0900

    ストリーム型

--- form/test.php
+++ form/test.php
@@ -1,13 +1,9 @@
 <?php 
 
-    // ファイル読み込み
-    $contentfile = '.read.dat';
-    $read = file_get_contents($contentfile);
-    echo $read;
+    $contactFile = '.read.dat';
+    $contents = fopen($contactFile, 'a+');
+    $addText = '1行追記' . "/n";
+    fwrite($contents, $addText);
+    fclose($contents);
 
-    // ファイル書き込み（上書き）
-    // file_put_contents($contentfile, 'テストです');
-
-    // ファイル書き込み（追記）
-    file_put_contents($contentfile, 'テストです', FILE_APPEND);
 ?>
