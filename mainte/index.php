d6896ea25018d3d3b161b09b6137e3629cb284db
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-28 15:26:14 +0900

    データの表示

---
+++ mainte/index.php
@@ -0,0 +1,27 @@
+<?php
+
+    require 'db_connection.php';
+
+    // ユーザー入力なし query
+    // $sql = 'select * from contacts where id = 1';
+    // $stmt = $pdo->query($sql);
+
+    // $result = $stmt -> fetchall();
+  
+    // echo "<pre>";
+    // var_dump($result);
+    // echo "</pre>";
+
+    // ユーザー入力あり（フォーム・検索機能） prepare bind esecut 悪意あるユーザーからデータを守る為 SQLインジェクション対策
+
+    $sql = 'select * from contacts where id = :id';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue('id', 2, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $result = $stmt -> fetchall();
+
+    echo "<pre>";
+    var_dump($result);
+    echo "</pre>";
+?>
